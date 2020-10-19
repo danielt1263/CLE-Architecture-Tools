88471faf6435ce7c3eeac9e0971cae23d58af74d
Daniel Tartaglia <danielt1263@gmail.com>
2020-10-18 21:29:44 -0400

    Add disposeBag to view controller.

--- File Templates/Rx Screen - xib.xctemplate/___FILEBASENAME___ViewController.swift
+++ File Templates/Rx Screen - xib.xctemplate/___FILEBASENAME___ViewController.swift
@@ -1,9 +1,11 @@
 //___FILEHEADER___
 
+import RxSwift
 import UIKit
 
 class ___VARIABLE_productName___ViewController: UIViewController, Configurable {
 
 	// IBOutlets go here
 
+	let disposeBag = DisposeBag()
 }
