5da65e98a6832de88c6259f30b154ca1475a3907
Daniel Tartaglia <danielt1263@gmail.com>
2020-11-07 08:57:28 -0500

    Refine Presenter.swift

--- Utilities/Presenter.swift
+++ Utilities/Presenter.swift
@@ -13,25 +13,65 @@ struct Scene<Action> {
 	let action: Observable<Action>
 }
 
-// shortcuts for creating scenes and view controllers from storyboards.
+/// shortcuts for creating scenes and view controllers.
 extension Presentable where Self: UIViewController {
+	/**
+	Create a scene by reconstituting an instance of this view controller from a storyboard with the same name as it.
+
+	- Parameter connect: A function describing how the view controller should be connected and returning an Observable that emits any data the scene needs to communicate to its parent.
+	- Returns: A Scene containing the view controller and return value of the connect function.
+
+	Example:
+
+	`let exampleScene = ExampleViewController.scene { $0.connect() }`
+	*/
 	static func scene<Action>(_ connect: (Self) -> Observable<Action>) -> Scene<Action> {
 		let storyboard = UIStoryboard(name: String(describing: self), bundle: nil)
 		let controller = storyboard.instantiateInitialViewController() as! Self
 		return controller.scene(connect)
 	}
 
+	/**
+	Extract an instance of this view controller out of a storyboard with the same name as it and cofigure it using the supplied connect function.
+
+	- Parameter connect: A function describing how the view controller should be connected.
+	- Returns: A configured view controller.
+
+	Example:
+
+	`let exampleViewController = ExampleViewController.create { $0.connect() }`
+	*/
 	static func create(_ connect: (Self) -> Void) -> UIViewController {
 		let storyboard = UIStoryboard(name: String(describing: self), bundle: nil)
 		let controller = storyboard.instantiateInitialViewController() as! Self
 		return controller.configure(connect)
 	}
 
+	/**
+	Create a scene from an already existing view controller.
+
+	- Parameter connect: A function describing how the view controller should be connected and returning an Observable that emits any data the scene needs to communicate to its parent.
+	- Returns: A Scene containing the view controller and return value of the connect function.
+
+	Example:
+
+	`let exampleScene = ExampleViewController().scene { $0.connect() }`
+	*/
 	func scene<Action>(_ connect: (Self) -> Observable<Action>) -> Scene<Action> {
 		loadViewIfNeeded()
 		return Scene(controller: self, action: connect(self))
 	}
 
+	/**
+	Configure an existing view controller using the supplied connect function.
+
+	- Parameter connect: A function describing how the view controller should be connected.
+	- Returns: A configured view controller.
+
+	Example:
+
+	`let exampleViewController = ExampleViewController().create { $0.connect() }`
+	*/
 	func configure(_ connect: (Self) -> Void) -> UIViewController {
 		loadViewIfNeeded()
 		connect(self)
@@ -39,27 +79,97 @@ extension Presentable where Self: UIViewController {
 	}
 }
 
-// shortcuts for presenting view controllers from other view controllers.
+/// shortcuts for presenting view controllers from other view controllers.
 extension UIViewController {
 
+	/**
+	Present a scene on this view controller. The scene will not be created until needed and its view controller will be dismissed when the action observable completes.
+
+	- Parameters:
+	- animated: Should the view controller present/dismiss in an animated style?
+	- scene: The scene that should be presented.
+	- Returns: The action observable of the scene.
+
+	Example:
+
+	```
+	someObservable
+	.flatMapFirst { [unowned self] someData in
+		self.present(animated: true, scene: ExampleViewController().scene { $0.connect(initialData: someData) })
+	}
+	```
+	*/
 	func present<T>(animated: Bool, scene: @autoclosure @escaping () -> Scene<T>) -> Observable<T> {
 		observable(for: scene(), show: self.presenter(animated: animated), remove: self.dismisser(animated: animated))
 	}
 
-	func present<T>(animated: Bool, popoverSource: UIView, scene: @autoclosure @escaping () -> Scene<T>) -> Observable<T> {
-		observable(for: scene(), show: self.presenter(animated: animated, popoverSource: popoverSource), remove: self.dismisser(animated: animated))
+	/**
+	Present a scene on this view controller in a popover if on iPad. The scene will not be created until needed and its view controller will be dismissed when the action observable completes.
+
+	- Parameters:
+	- animated: Should the view controller present/dismiss in an animated style?
+	- scene: The scene that should be presented.
+	- Returns: The action observable of the scene.
+
+	Example:
+
+	```
+	someObservable
+	.flatMapFirst { [unowned self, unowned myButton] someData in
+		self.present(animated: true, overSourceView: myButton, scene: ExampleViewController().scene { $0.connect(initialData: someData) })
+	}
+	```
+	*/
+	func present<T>(animated: Bool, overSourceView sourceView: UIView, scene: @autoclosure @escaping () -> Scene<T>) -> Observable<T> {
+		observable(for: scene(), show: self.presenter(animated: animated, overSourceView: sourceView), remove: self.dismisser(animated: animated))
 	}
 
+	/**
+	Create an Observer that will present a scene on this view controller when it receives a value. The scene will not be created until needed and its view controller will be dismissed when the action observable completes. Any next events emitted from the action will be ignored.
+
+	- Parameters:
+	- animated: Should the view controller present/dismiss in an animated style?
+	- scene: The scene that should be presented.
+	- Returns: The action observable of the scene.
+
+	Example:
+
+	```
+	someObservable
+		.bind(to: present(animated: true) { someData in
+			ExampleViewController().scene { $0.connect(initialData: someData) }
+		})
+	}
+	```
+	*/
 	func present<T, U>(animated: Bool, scene: @escaping (T) -> Scene<U>) -> AnyObserver<T> {
 		observer(for: scene, show: self.presenter(animated: animated), remove: self.dismisser(animated: true))
 	}
 
+	/**
+	Create an Observer that will present a view controller on this view controller when it receives a value. The view controller must handle its own dismissal.
+
+	- Parameters:
+	- animated: Should the view controller present/dismiss in an animated style?
+	- scene: The scene that should be presented.
+	- Returns: The action observable of the scene.
+
+	Example:
+
+	```
+	someObservable
+		.bind(to: present(animated: true) { someData in
+			ExampleViewController().configure { $0.connect(initialData: someData) }
+		})
+	}
+	```
+	*/
 	func present<T>(animated: Bool, create: @escaping (T) -> UIViewController) -> AnyObserver<T> {
 		observer(for: create, show: self.presenter(animated: animated))
 	}
 }
 
-// shortcuts for pushing view controllers from other view controllers.
+/// shortcuts for pushing view controllers from other view controllers.
 extension UINavigationController {
 
 	func push<T>(animated: Bool, scene: @autoclosure @escaping () -> Scene<T>) -> Observable<T> {
@@ -86,8 +196,8 @@ extension UINavigationController {
 		return { [unowned self] in
 			weak var controller = $0
 			return Completable.create { observer in
-				if self.topViewController === controller {
-					self.popViewController(animated: true)
+				if let position = self.viewControllers.firstIndex(where: { $0 === controller }), position > 0 {
+					self.popToViewController(self.viewControllers[position - 1], animated: animated)
 				}
 				return Disposables.create()
 			}
@@ -97,7 +207,7 @@ extension UINavigationController {
 
 extension UIViewController {
 
-	func presenter(animated: Bool, popoverSource sourceView: UIView) -> (UIViewController) -> Void {
+	func presenter(animated: Bool, overSourceView sourceView: UIView) -> (UIViewController) -> Void {
 		return { [weak self] controller in
 			if let popoverPresentationController = controller.popoverPresentationController {
 				popoverPresentationController.sourceView = sourceView
@@ -114,13 +224,18 @@ extension UIViewController {
 	}
 
 	func dismisser(animated: Bool) -> (UIViewController) -> Completable {
-		return { [unowned self] in
+		return {
 			weak var controller = $0
 			return Completable.create { observer in
-				if self.presentedViewController === controller {
-					self.dismiss(animated: animated) {
+				if let parent = controller?.presentingViewController {
+					parent.dismiss(animated: animated) {
 						observer(.completed)
 					}
+				}
+				else if let this = controller {
+					this.dismiss(animated: true, completion: {
+						observer(.completed)
+					})
 				}
 				else {
 					observer(.completed)
@@ -186,7 +301,4 @@ func observer<T>(for create: @escaping (T) -> UIViewController, show: @escaping 
 }
 
 protocol Presentable { }
-
-extension UIAlertController: Presentable { }
-extension UINavigationController: Presentable { }
-extension UITabBarController: Presentable { }
+extension UIViewController: Presentable { }
