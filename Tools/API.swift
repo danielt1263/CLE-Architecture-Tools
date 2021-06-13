29e0e77097f24dbe9eca9978721d34ee3b8c6028
Daniel Tartaglia <danielt1263@gmail.com>
2021-06-13 10:35:50 -0400

    Update the optional API tool.

--- Tools/API.swift
+++ Tools/API.swift
@@ -1,24 +1,25 @@
 //
-//  Resource.swift
+//  API.swift
 //
 //  Created by Daniel Tartaglia on 3/4/21.
 //  Copyright © 2021 Daniel Tartaglia. MIT License.
 //
 
-import Foundation
 import RxSwift
+import Foundation
+import Cause_Logic_Effect
 
-struct Endpoint<T> {
+public struct Endpoint<T> {
 	let request: URLRequest
 	let response: (Data) throws -> T
 }
 
-final class API {
+public final class API {
 	private let session: URLSession
 	private let activityIndicator: ActivityIndicator
 	private let errorRouter: ErrorRouter
 
-	init(session: URLSession, activityIndicator: ActivityIndicator, errorRouter: ErrorRouter) {
+	public init(session: URLSession = .shared, activityIndicator: ActivityIndicator = ActivityIndicator(), errorRouter: ErrorRouter = ErrorRouter()) {
 		self.session = session
 		self.activityIndicator = activityIndicator
 		self.errorRouter = errorRouter
@@ -32,8 +33,57 @@ final class API {
 	}
 }
 
+public protocol EncoderType {
+	var conentType: URLRequest.ContentType { get }
+	func encode<T>(_ value: T) throws -> Data where T : Encodable
+}
+
+public protocol DecoderType {
+	func decode<T: Decodable >(_ type: T.Type, from data: Data) throws -> T
+}
+
+extension URLRequest {
+	public enum Method: String {
+		case get = "GET"
+		case post = "POST"
+		case put = "PUT"
+		case patch = "PATCH"
+		case delete = "DELETE"
+	}
+
+	public enum ContentType: String {
+		case json = "application/json"
+		case xml = "application/xml"
+		case urlencoded = "application/x-www-form-urlencoded"
+	}
+
+	public init(_ method: Method, url: URL, accept: ContentType? = nil, contentType: ContentType? = nil, body: Data? = nil, headers: [String: String] = [:], query: [String: String] = [:]) {
+		var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+		components.queryItems = query.map { URLQueryItem(name: $0.0, value: $0.1) }
+		var request = URLRequest(url: components.url!)
+		if let accept = accept {
+			request.setValue(accept.rawValue, forHTTPHeaderField: "Accept")
+		}
+		if let contentType = contentType {
+			request.setValue(contentType.rawValue, forHTTPHeaderField: "Content-Type")
+		}
+		for (key, value) in headers {
+			request.setValue(value, forHTTPHeaderField: key)
+		}
+		request.httpMethod = method.rawValue
+
+		// body *needs* to be the last property that we set, because of this bug: https://bugs.swift.org/browse/SR-6687
+		request.httpBody = body
+		self = request
+	}
+
+	public init<B: Encodable>(encoder: EncoderType, method: Method, url: URL, accept: ContentType?, body: B, headers: [String: String] = [:], query: [String: String] = [:]) {
+		self.init(method, url: url, accept: accept, contentType: encoder.conentType, body: try! encoder.encode(body), headers: headers, query: query)
+	}
+}
+
 extension Endpoint where T: Decodable {
-	init(request: URLRequest, decoder: JSONDecoder) {
+	public init(request: URLRequest, decoder: DecoderType) {
 		self.request = request
 		self.response = { try decoder.decode(T.self, from: $0) }
 	}
@@ -45,3 +95,18 @@ extension Endpoint where T == Void {
 		self.response = { _ in }
 	}
 }
+
+extension JSONEncoder: EncoderType {
+	public var conentType: URLRequest.ContentType {
+		.json
+	}
+}
+
+extension PropertyListEncoder: EncoderType {
+	public var conentType: URLRequest.ContentType {
+		.xml
+	}
+}
+
+extension JSONDecoder: DecoderType { }
+extension PropertyListDecoder: DecoderType { }
