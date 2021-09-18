dbdd2ed0f48e453b86142d8fb21e0eb3c5ed75d7
Daniel Tartaglia <danielt1263@gmail.com>
2021-09-18 10:11:10 -0400

    Update reaction functions.

--- Utilities/Cycle.swift
+++ Utilities/Cycle.swift
@@ -17,36 +17,23 @@ public func cycle<Input, Output>(logic: @escaping (Observable<Input>) -> Observa
 }
 
 public func react<State, Request, Action>(request: @escaping (State) -> Request?, effect: @escaping (Request) -> Observable<Action>) -> (Observable<State>) -> Observable<Action> where Request: Equatable {
+	react(request: request, compare: { $0 == $1 }, effect: effect)
+}
+
+public func react<State, Action>(request: @escaping (State) -> Bool, effect: Observable<Action>) -> (Observable<State>) -> Observable<Action> {
 	{ $0.map(request)
 		.distinctUntilChanged()
-		.compactMap { $0 }
-		.flatMap(effect)
+		.flatMapLatest { $0 ? effect : Observable.empty() }
 	}
 }
 
-public func reactLatest<State, Request, Action>(request: @escaping (State) -> Request?, effect: @escaping (Request) -> Observable<Action>) -> (Observable<State>) -> Observable<Action> where Request: Equatable {
+public func react<State, Request, Action>(request: @escaping (State) -> Request?, compare: @escaping (Request?, Request?) -> Bool, effect: @escaping (Request) -> Observable<Action>) -> (Observable<State>) -> Observable<Action> {
 	{ $0.map(request)
-		.distinctUntilChanged()
+		.distinctUntilChanged(compare)
 		.flatMapLatest { (request) -> Observable<Action> in
 			guard let request = request else { return Observable.empty() }
 			return effect(request)
 		}
-	}
-}
-
-public func reactFirst<State, Request, Action>(request: @escaping (State) -> Request?, effect: @escaping (Request) -> Observable<Action>) -> (Observable<State>) -> Observable<Action> where Request: Equatable {
-	{ $0.map(request)
-		.distinctUntilChanged()
-		.compactMap { $0 }
-		.flatMapFirst(effect)
-	}
-}
-
-public func reactConcat<State, Request, Action>(request: @escaping (State) -> Request?, effect: @escaping (Request) -> Observable<Action>) -> (Observable<State>) -> Observable<Action> where Request: Equatable {
-	{ $0.map(request)
-		.distinctUntilChanged()
-		.compactMap { $0 }
-		.concatMap(effect)
 	}
 }
 
