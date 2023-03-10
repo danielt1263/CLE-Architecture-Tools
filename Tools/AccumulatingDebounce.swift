2928bc54d13862d5bb83904abaf847bb90adaf93
Daniel Tartaglia <daniel.tartaglia@zealitconsultants.com>
2023-03-10 15:08:17 -0500

    Format cleanup.

--- Tools/AccumulatingDebounce.swift
+++ Tools/AccumulatingDebounce.swift
@@ -9,42 +9,40 @@ import Foundation
 import RxSwift
 
 extension ObservableType {
-    /**
-     Accumulates elements from an observable sequence where are followed by another element within a specified relative time
-     duration, using the specified scheduler to run throttling timers.
+	/**
+	 Accumulates elements from an observable sequence where are followed by another element within a specified relative time
+	 duration, using the specified scheduler to run throttling timers.
 
-     - Parameters:
-     - dueTime: Throttling duration for each element.
-     - scheduler:  Scheduler to run the throttle timers on.
-     - Returns: The throttled sequence.
-     */
-    func accumulatingDebounce(_ dueTime: RxTimeInterval, scheduler: SchedulerType) -> Observable<[Element]> {
-        Observable.create { observer in
-            var state = [Element]()
-            var nextEmit: Disposable? = nil
-            let lock = NSRecursiveLock()
-            let disposable = self.subscribe { event in
-                lock.lock(); defer { lock.unlock() }
-                switch event {
-                case .next(let element):
-                    state.append(element)
-                    nextEmit?.dispose()
-                    nextEmit = scheduler.scheduleRelative((), dueTime: dueTime, action: {
-                        lock.lock(); defer { lock.unlock() }
-                        observer.onNext(state)
-                        state = []
-                        return Disposables.create()
-                    })
-                case .error(let error):
-                    observer.onError(error)
-                case .completed:
-                    if !state.isEmpty {
-                        observer.onNext(state)
-                    }
-                    observer.onCompleted()
-                }
-            }
-            return Disposables.create(disposable, nextEmit ?? Disposables.create())
-        }
-    }
+	 - Parameters:
+	 - dueTime: Throttling duration for each element.
+	 - scheduler:  Scheduler to run the throttle timers on.
+	 - Returns: The throttled sequence.
+	 */
+	func accumulatingDebounce(_ dueTime: RxTimeInterval, scheduler: SchedulerType) -> Observable<[Element]> {
+		.create { observer in
+			var state = [Element]()
+			var nextEmit = Disposable?.none
+			let lock = NSRecursiveLock()
+			let disposable = self.subscribe { event in
+				lock.lock(); defer { lock.unlock() }
+				switch event {
+				case .next(let element):
+					state.append(element)
+					nextEmit?.dispose()
+					nextEmit = scheduler.scheduleRelative((), dueTime: dueTime) {
+						lock.lock(); defer { lock.unlock() }
+						observer.onNext(state)
+						state = []
+						return Disposables.create()
+					}
+				case .error(let error):
+					observer.onError(error)
+				case .completed:
+					if !state.isEmpty { observer.onNext(state) }
+					observer.onCompleted()
+				}
+			}
+			return Disposables.create(disposable, nextEmit ?? Disposables.create())
+		}
+	}
 }
